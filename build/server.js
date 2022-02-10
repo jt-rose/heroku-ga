6f060c3606122fd166d649ea2fa034aad47f9e16
Jeffrey Rose <jtr219@outlook.com>
2022-02-10 17:27:46 -0500

    set up async main function

--- build/server.js
+++ build/server.js
@@ -1,3 +1,39 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 //___________________
 //Dependencies
 //___________________
@@ -5,45 +41,46 @@ import express from "express";
 import methodOverride from "method-override";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
-var app = express();
-var db = mongoose.connection;
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-var PORT = process.env.PORT || 3003;
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-var MONGODB_URI = process.env.MONGODB_URI;
-// Connect to Mongo &
-// Fix Depreciation Warnings from Mongoose
-// May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI);
-// Error / success
-db.on("error", function (err) { return console.log(err.message + " is Mongod not running?"); });
-db.on("connected", function () { return console.log("mongo connected: ", MONGODB_URI); });
-db.on("disconnected", function () { return console.log("mongo disconnected"); });
-//___________________
-//Middleware
-//___________________
-//use public folder for static assets
-app.use(express.static("public"));
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
-app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
-//use method override
-app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
-//___________________
-// Routes
-//___________________
-//localhost:3000
-app.get("/", function (req, res) {
-    res.send("Hello World!");
-});
-//___________________
-//Listener
-//___________________
-app.listen(PORT, function () { return console.log("Listening on port:", PORT); });
+var main = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var app, db, PORT, MONGODB_URI;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, dotenv.config()];
+            case 1:
+                _a.sent();
+                app = express();
+                db = mongoose.connection;
+                PORT = process.env.PORT || 3003;
+                MONGODB_URI = process.env.MONGODB_URI;
+                // Connect to Mongo
+                mongoose.connect(MONGODB_URI);
+                // Error / success
+                db.on("error", function (err) { return console.log(err.message + " is Mongod not running?"); });
+                db.on("connected", function () { return console.log("mongo connected: ", MONGODB_URI); });
+                db.on("disconnected", function () { return console.log("mongo disconnected"); });
+                //___________________
+                //Middleware
+                //___________________
+                //use public folder for static assets
+                app.use(express.static("public"));
+                // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+                app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
+                app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
+                //use method override
+                app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
+                //___________________
+                // Routes
+                //___________________
+                //localhost:3000
+                app.get("/", function (_req, res) {
+                    res.send("Hello World!");
+                });
+                //___________________
+                //Listener
+                //___________________
+                app.listen(PORT, function () { return console.log("Listening on port:", PORT); });
+                return [2 /*return*/];
+        }
+    });
+}); };
+main().catch(function (err) { return console.log(err); });
--- src/server.ts
+++ src/server.ts
@@ -5,54 +5,57 @@ import express from "express";
 import methodOverride from "method-override";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
-const app = express();
-const db = mongoose.connection;
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3003;
 
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI;
+const main = async () => {
+  await dotenv.config();
+  const app = express();
+  const db = mongoose.connection;
+  //___________________
+  //Port
+  //___________________
+  // Allow use of Heroku's port or your own local port, depending on the environment
+  const PORT = process.env.PORT || 3003;
 
-// Connect to Mongo &
-// Fix Depreciation Warnings from Mongoose
-// May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI as string);
+  //___________________
+  //Database
+  //___________________
+  // How to connect to the database either via heroku or locally
+  const MONGODB_URI = process.env.MONGODB_URI;
 
-// Error / success
-db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
-db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
-db.on("disconnected", () => console.log("mongo disconnected"));
+  // Connect to Mongo
+  mongoose.connect(MONGODB_URI as string);
 
-//___________________
-//Middleware
-//___________________
+  // Error / success
+  db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+  db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+  db.on("disconnected", () => console.log("mongo disconnected"));
 
-//use public folder for static assets
-app.use(express.static("public"));
+  //___________________
+  //Middleware
+  //___________________
 
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
-app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
+  //use public folder for static assets
+  app.use(express.static("public"));
 
-//use method override
-app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
+  // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+  app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
+  app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
 
-//___________________
-// Routes
-//___________________
-//localhost:3000
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  //use method override
+  app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
 
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log("Listening on port:", PORT));
+  //___________________
+  // Routes
+  //___________________
+  //localhost:3000
+  app.get("/", (_req, res) => {
+    res.send("Hello World!");
+  });
+
+  //___________________
+  //Listener
+  //___________________
+  app.listen(PORT, () => console.log("Listening on port:", PORT));
+};
+
+main().catch((err) => console.log(err));
