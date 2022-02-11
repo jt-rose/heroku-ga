648b91a6bb4c07c516a8d3474792bc9bfbb3e156
Jeffrey Rose <jtr219@outlook.com>
2022-02-11 00:11:04 -0500

    set up Connect model

---
+++ src/models/Connect.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { ObjectId } from "./ObjectId";
+
+interface IConnect {
+  from: typeof ObjectId; // mongo id - string?
+  to: typeof ObjectId;
+  date: Date; // refers to either date invite sent or date accepted, depending on status
+  message: string;
+  targetLanguage: string; // constant
+  inviteAccepted: boolean;
+}
+
+const connectSchema = new mongoose.Schema<IConnect>({
+  from: { type: ObjectId, required: true },
+  to: { type: ObjectId, required: true },
+  date: { type: Date, required: true },
+  message: { type: String, required: true },
+  targetLanguage: { type: String, required: true },
+  inviteAccepted: { type: Boolean, required: true },
+});
+
+export const Connect = mongoose.model("Connect", connectSchema);
