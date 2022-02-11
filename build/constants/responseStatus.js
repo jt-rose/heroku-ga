19c18f27a8c1cd42b1b8d2d9faeff9911c532ceb
Jeffrey Rose <jtr219@outlook.com>
2022-02-11 02:56:04 -0500

    add response status

---
+++ build/constants/responseStatus.js
@@ -0,0 +1,4 @@
+export var NO_RESPONSE = "NO_RESPONSE";
+export var ACCEPTED = "ACCEPTED";
+export var DECLINED = "DECLINED";
+export var responseStatus = [NO_RESPONSE, ACCEPTED, DECLINED];
---
+++ src/constants/responseStatus.ts
@@ -0,0 +1,5 @@
+export const NO_RESPONSE = "NO_RESPONSE";
+export const ACCEPTED = "ACCEPTED";
+export const DECLINED = "DECLINED";
+
+export const responseStatus = [NO_RESPONSE, ACCEPTED, DECLINED];
